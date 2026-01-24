fadea0f441322ef208cefd1b48c1def6c55d8809
Rafi <87369942+Restartor@users.noreply.github.com>
2026-01-24 13:26:22 +0700

    Add comments for car showroom system structure and functions
    Added comments outlining the structure and functions for a car showroom system.

--- test.cpp
+++ test.cpp
@@ -1,1 +1,14 @@
-ifififigui
+// Membuat sistem showroom mobil dengan autoid setiap data mobil ditambahkan
+
+//struktur showroom (autoid, merk, nama, tahun, harga)
+
+
+// void fungsi menambahkan data mobil
+
+
+// void fungsi melihat semua data mobil
+
+// void fungsi menghapus data mobil berdasarkan id
+
+// int main
+// do {} while(pilihan != 4) return 0;
